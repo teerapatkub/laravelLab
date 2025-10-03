3925ea558942962c6ebdce6ad2b79dcfacacfb63
suphachaikh-creator <suphachai.kh@kkumail.com>
2025-10-04 03:42:11 +0700

    Update authentication-card.blade.php

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,13 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
-
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div style="
+    width: 100%;
+    max-width: 28rem;
+    margin-top: 1.5rem;
+    padding: 1.5rem 1.5rem;
+    background-color: #26333A;
+    border-radius: 0.5rem;
+    overflow: hidden;
+">
         {{ $slot }}
     </div>
 </div>
