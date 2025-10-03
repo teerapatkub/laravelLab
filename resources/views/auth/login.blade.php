b8b82d15bc057f49e8c3a264fefd462c954a5373
suphachaikh-creator <suphachai.kh@kkumail.com>
2025-10-04 03:38:49 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,5 @@
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -14,35 +10,38 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
+                <x-label for="email" value="{{ __('อีเมล') }}"/>
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('รหัสผ่าน') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-white hover:text-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" 
+                    href="{{ route('password.request') }}">
+                    {{ __('ลืมรหัสผ่าน') }}
+                </a>
                 @endif
+            </div>
 
-                <x-button class="ms-4">
+            <div class="mt-5 text-center">
+                <x-button>
                     {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
+        <div>
+            <h5 class="mt-4 mb-4 text-center text-white">หากคุณยังไม่เป็นสมาชิก</h5>
+                <a href="{{ route('register') }}">
+                    <x-button>
+                        {{__('สมัครสมาชิก') }}
+                    </x-button>
+                </a>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
