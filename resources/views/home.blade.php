7bd490812f6c51c83a51e2381ae48a4d685d92f0
suphachaikh-creator <suphachai.kh@kkumail.com>
2025-10-04 22:29:56 +0700

    ีupdate

---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar Local Icon</title>
+    <link rel="stylesheet" href="fonts/fontisto/fontisto.min.css">
+    <style>
+        body {
+        margin: 0;
+        padding: 0;
+        background-color: #f5e3e7;
+        }
+
+        .a1 {
+            background-color: #ffffffff;
+            height: 50px;
+            display: flex;
+            align-items: center;
+            padding: 0 5px;
+        }
+
+        .a1 a {
+            color: #fff;
+            margin-right: 50px;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+        .user-icon {
+            margin-left: auto;
+        }
+
+    </style>
+</head>
+<body>
+
+<nav>
+    <div class="a1">
+        <a href=""></i> หน้าแรก</a>
+        <a href=""></i> ภาพยนตร์</a>
+        <a href=""></i> ประวัติการเข้าชม</a>
+
+        <a href="" class="user-icon">
+            <img src="Icon/circle-user.png" alt="User Icon" width="24" height="24" >
+        </a>    
+    </div>
+</nav>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/movies', [MovieController::class, 'index']);
