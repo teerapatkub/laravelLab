54636b3222294377c198bc5629f4b827e2892e6f
teerapatkub <teerapat.boo@kkumail.com>
2025-10-04 21:49:44 +0700

    update

---
+++ storage/framework/views/1b59183fbb58520f931d16a88f6c8053.php
@@ -0,0 +1,6 @@
+<button <?php echo e($attributes->merge(['type' => 'submit', 
+    'class' => 'w-full bg-red-600 justify-center text-white'])); ?>>
+    <?php echo e($slot); ?>
+
+</button>
+<?php /**PATH C:\xampp\htdocs\laravelLab\resources\views/components/button.blade.php ENDPATH**/ ?>
