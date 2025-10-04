43d3a6123fb6f2a27e3f4ed4e9b1dba75692b774
suphachaikh-creator <suphachai.kh@kkumail.com>
2025-10-04 23:22:54 +0700

    u

---
+++ storage/framework/views/58960ebfc93929385e5062dd53f8d4ce.php
@@ -0,0 +1,15 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div style="
+    width: 100%;
+    max-width: 28rem;
+    margin-top: 1.5rem;
+    padding: 1.5rem 1.5rem;
+    background-color: #26333A;
+    border-radius: 0.5rem;
+    overflow: hidden;
+">
+        <?php echo e($slot); ?>
+
+    </div>
+</div>
+<?php /**PATH C:\Users\WINDOWS\OneDrive\เอกสาร\GitHub\laravelLab\resources\views/components/authentication-card.blade.php ENDPATH**/ ?>
